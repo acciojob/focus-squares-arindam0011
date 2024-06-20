ebc67eb831274ea659728bee574a63cc86c055ea
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-20 22:13:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const divs=document.querySelectorAll(".square");
+
+divs.forEach(el=>{
+	el.addEventListener("mouseover", function(event){
+		divs.forEach(div=>{
+			if(div!=event.target){
+				div.style.backgroundColor='#6F4E37'
+			}
+		});
+		
+	})
+	el.addEventListener("mouseout", function(){
+		divs.forEach((div) => {
+		    div.style.backgroundColor = '#E6E6FA'; // lavender
+	    });
+	})
+})
